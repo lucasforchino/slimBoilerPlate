f4d6a5ca49fba7334b0c0130dae87be510d5b4fb
Lucas Forchino <lucasforchino@gmail.com>
2013-11-23 12:59:36 -0300

    remove description

--- App/Templates/_layouts/layout.php
+++ App/Templates/_layouts/layout.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <title></title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="All analytics in one place, your phone.">
+        <meta name="description" content="">
         <meta name="author" content="">
     </head>
 
